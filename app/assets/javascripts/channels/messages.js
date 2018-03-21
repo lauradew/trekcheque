30181b3519a81902daa35e96bc86bab031c49c5f
Laura De Witt <lauramdewitt@gmail.com>
2018-03-21 14:13:26 +0000

    added validation to messages

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,7 +1,9 @@
 App.messages = App.cable.subscriptions.create('MessagesChannel', {
     received: function (data) {
-        $("#messages").removeClass('hidden')
-        return $('#messages').append(this.renderMessage(data));
+        if (data.trip === tripId) {
+            $("#messages").removeClass('hidden')
+            return $('#messages').append(this.renderMessage(data));
+        }
     },
 
     renderMessage: function (data) {
